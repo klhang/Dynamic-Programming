3c2ea4f7e9b99ab0a57103047b66932d045e7eda
Klhang <chrisjunhang@gmail.com>
2017-10-26 12:06:01 -0700

    update solutions

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -1,118 +1,124 @@
-require "byebug"
-
 class DynamicProgramming
+  attr_accessor :blair_cache
 
   def initialize
-    @blair_cache = { 1 => 1, 2 => 2 }
-    @frog_cache = [[], [[1]], [[1,1],[2]], [[1,1,1],[1,2],[2,1],[3]]]
-    @super_frog_cache = [[], [[1]]]
+    @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = [[[]], [[1]], [[1, 1], [2]]]
   end
 
-  # 1, 2, 6, 13
   def blair_nums(n)
     return @blair_cache[n] if @blair_cache[n]
-
-    ans = blair_nums(n - 1) + blair_nums(n - 2) + ((n - 1) * 2) - 1
-    @blair_cache[n] = ans
-    ans
-  end
-
-  def build_blair_nums_cache(n)
-    cache = { 1 => 1, 2 => 2 }
-    (3..n).each do |i|
-      cache[i] = cache[i - 1] + cache[i - 2] + ((i - 1) * 2) - 1
+    odd_num = 1
+    (1...n-1).each do |i|
+      odd_num += 2
     end
-    cache
-  end
-
-  def blair_nums_bu(n)
-    cache = build_blair_nums_cache(n)
-    cache[n]
+    @blair_cache[n] = blair_nums(n-1) + blair_nums(n-2) + odd_num
   end
 
   def frog_hops_bottom_up(n)
-    cache = frog_cache_builder(n)
-    cache
+    frog_cache_builder(n)[n]
   end
 
   def frog_cache_builder(n)
-    return @frog_cache[n] if @frog_cache[n]
+    ways_collection = [[[]], [[1]], [[1, 1], [2]]]
 
-    # [[1]] << 3 -> [[1,3]]
-    # [[1,1],[2]] << 2 -> [[1,1,2],[2,2]]
-    # [[1,1,1],[1,2],[2,1],[3]] << 1 -> [[1,1,1,1],[1,2,1],[2,1,1],[3,1]]
+    return ways_collection if n < 3
 
-    (4..n).each do |i|
-      ans = []
-
-      (1..3).each do |offset|
-        @frog_cache[i - offset].each do |steps|
-          ans << steps + [offset]
+    (3..n).each do |i|
+      new_way_set = []
+      (1..3).each do |first_step|
+        ways_collection[i - first_step].each do |way|
+          new_way = [first_step]
+          way.each do |step|
+            new_way << step
+          end
+          new_way_set << new_way
         end
       end
-
-      @frog_cache[i] = ans
+      ways_collection << new_way_set
     end
 
-    @frog_cache[n]
+    ways_collection
   end
 
   def frog_hops_top_down(n)
+    @frog_cache = [[[]], [[1]], [[1, 1], [2]]]
     frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
     return @frog_cache[n] if @frog_cache[n]
-
-    ans = []
-    (1..3).each do |offset|
-      frog_hops_top_down_helper(n - offset).each do |steps|
-        ans << steps + [offset]
+    new_way_set = []
+    (1..3).each do |first_step|
+      frog_hops_top_down_helper(n - first_step).each do |way|
+        new_way = [first_step]
+        way.each do |step|
+          new_way << step
+        end
+        new_way_set << new_way
       end
     end
-    @frog_cache[n] = ans
-
-    ans
+    @frog_cache[n] = new_way_set
   end
 
-  # max jumps = 4
-  # []
-  # [1]
-  # [[1,1],[2]]
-  # [[1,1,1],[2,1],[1,2],[3]]
-  # [[1,1,1,1],[2,1,1],[1,2,1],[3,1][1,1,2],[2,2],[1,3],[4]]
+  def super_frog_hops(n, k)
+    ways_collection = [[[]], [[1]]]
 
-  # [[1,1,1,1,1],[2,1,1,1],[1,2,1,1],[3,1,1][1,1,2,1],[2,2,1],[1,3,1],[4,1]]
-  # [[1,1,1,2],[2,1,2],[1,2,2],[3,2][1,1,3],[2,3],[1,4],[4]]
+    return ways_collection[n] if n < 2
 
-  # max jump = 2
-  # []
-  # [1]
-  # [[1,1],[2]]
-  # [[1,1,1],[2,1],[1,2],[2]] Only increment if last element is < max jump
-
-  def super_frog_hops(n, max_jump)
-    return [[]] if n == 0
-    return [[1]] if n == 1
-
-    ans = []
-
-    super_frog_hops(n - 1, max_jump).each do |steps|
-      ans << steps + [1]
-      ans << steps[0...-1] + [steps.last + 1] if steps.last < max_jump
+    (2..n).each do |i|
+      new_way_set = []
+      (1..k).each do |first_step|
+        next if i - first_step < 0
+        ways_collection[i - first_step].each do |way|
+          new_way = [first_step]
+          way.each do |step|
+            new_way << step
+          end
+          new_way_set << new_way
+        end
+      end
+      ways_collection << new_way_set
     end
 
-    ans
+    ways_collection[n]
   end
 
   def knapsack(weights, values, capacity)
-    return 0 if capacity == 0
-    values[0]
+    return 0 if capacity == 0 || weights.length == 0
+    solution_table = knapsack_table(weights, values, capacity)
+    solution_table[capacity][-1]
   end
 
   # Helper method for bottom-up implementation
   def knapsack_table(weights, values, capacity)
+    solution_table = []
+    # Build solutions for knapsacks of increasing capacity
+    (0..capacity).each do |i|
+      solution_table[i] = []
+      # go through the weights one by one, by index
+      (0..weights.length - 1).each do |j|
+        if i == 0
+          # if the capacity is 0, then 0 is how much value can be placed in any slot
+          solution_table[i][j] = 0
+        elsif j == 0
+          # for the first item in our list, you must check for capacity
+          # if there is, then you enter its value in the first slot, otherwise 0
+          solution_table[i][j] = weights[0] > i ? 0 : values[0]
+        else
+          # the first option is the entry from considering the previous item at this capacity
+          option1 = solution_table[i][j - 1]
+          # the second option (assuming enough capacity) is the entry from a smaller bag
+          # (with enough room for this item) plus this item's value
+          option2 = i < weights[j] ? 0 : solution_table[i - weights[j]][j - 1] + values[j]
+          # the actual entry for this item is the optimum of the two choices
+          optimum = [option1, option2].max
+          solution_table[i][j] = optimum
+        end
+      end
+    end
 
+    solution_table
   end
 
   def maze_solver(maze, start_pos, end_pos)
