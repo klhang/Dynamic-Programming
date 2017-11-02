0d808d2b315a9c9aab8cad0c634595f86be05e2c
Klhang <chrisjunhang@gmail.com>
2017-11-01 17:17:09 -0700

    update solutions

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -20,7 +20,25 @@ class DynamicProgramming
   end
 
   def frog_cache_builder(n)
+    ways_collection = [[[]], [[1]], [[1, 1], [2]]]
 
+    return ways_collection if n < 3
+
+    (3..n).each do |i|
+      new_way_set = []
+      (1..3).each do |first_step|
+        ways_collection[i - first_step].each do |way|
+          new_way = [first_step]
+          way.each do |step|
+            new_way << step
+          end
+          new_way_set << new_way
+        end
+      end
+      ways_collection << new_way_set
+    end
+
+    ways_collection
   end
 
   def frog_hops_top_down(n)
